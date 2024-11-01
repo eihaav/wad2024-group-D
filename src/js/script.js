e39fcc2c502d1df27312ef69bedc4c51e04965c6
Anton Otrokov <anton.otroko@gmail.com>
2024-11-01 13:40:22 +0200

    homework

--- src/js/script.js
+++ src/js/script.js
@@ -83,6 +83,29 @@ function DateToHumanReadable(date){
     //console.log(humanReadable);
     return humanReadable;
 }
+
+// When the user clicks this button, a dropdown window is created
+function toggleDropdown() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  
+  if (!event.target.matches('.dropdown_button')) {
+    
+    var dropdowns = document.getElementsByClassName("dropdown_content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) { // closes dropdowns
+      
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 /*<article>
             <header class="postHeader">
               <img src="src/blank-profile-picture.webp" alt="Blank Profile Picture">

8dc5709848b4c4fd87bf455f0b5353276eef1926
Erik Ivar Haav <erikivar.haav@gmail.com>
2024-11-23 00:10:35 +0200

    Added posts for Vue

---
+++ vue_project/src/components/Posts.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="posts">
+        <Post v-for="post in humanReadablePosts" :key="post.id" :post="post"></Post>
+    </div>
+</template>
+
+<script>
+import Post from '@/components/Post.vue'
+
+export default {
+    name: 'Posts',
+    components: {
+        Post,
+    },
+    data: function () {
+        return {
+        }
+    },
+    computed: {
+        humanReadablePosts() {
+            return this.$store.getters.humanReadablePosts
+        }
+    }
+};
+</script>
+
+<style scoped>
+.posts {
+
+    margin-left: 1%;
+    margin-right: 1%;
+    margin-top: 0px;
+}
+</style>

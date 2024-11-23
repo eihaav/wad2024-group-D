35fb6c98f6d39958150b0044f49d43ee85450833
Anton Otrokov <anton.otroko@gmail.com>
2024-11-23 14:55:37 +0200

    fix

--- FirstProject/src/js/signupForm.js
+++ FirstProject/src/js/signupForm.js
@@ -1,36 +1,11 @@
-document.getElementById('signupForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-
-    let message = document.getElementById('message');
-    let passwordError = document.getElementById('passwordError');
-    let confirmPasswordError = document.getElementById('confirmPasswordError');
-
-    let passwordValid = validatePassword(password);
-    if (!passwordValid) {
-        passwordError.textContent = 'Password does not meet the requirements.';
-        return;
-    } else {
-        passwordError.textContent = '';
-    }
-
-    // Here you can add code to send the form data to the server
-
-    message.style.color = 'green';
-    message.textContent = 'Signup successful!';
-});
-
-
-function validatePassword(password) {
-    let uppercase = /[A-Z]/g; // Does the password have uppercase, lowercase, numeric characters.
-    let lowercase = /[a-z]/g; 
-    let numeric = /[0-9]/g; 
-    let special = '_';
-    if (password.length >= 8 && password.length < 15 && password.match(uppercase) && password.match(numeric) && password[0].match(uppercase) && password.match(special)) {
-
-        console.log("Password correct");
-    }
-    return password;
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
 }
+
+// Hide the box.
+myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+}
+
+
